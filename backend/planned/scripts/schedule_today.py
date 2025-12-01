2fa8231bf06ebbb43e126a6ef6c1c60c52c783eb
Todd Sifleet <todd.sifleet@gmail.com>
2025-12-01 15:54:44 -0600

    oops

--- backend/planned/scripts/schedule_today.py
+++ backend/planned/scripts/schedule_today.py
@@ -1,11 +1,11 @@
 import asyncio
 
-from planned.services import sheppard_svc
+from planned.services import day_svc
 from planned.utils.dates import get_current_date
 
 
 async def main():
-    result = await sheppard_svc.schedule_day(get_current_date())
+    result = await day_svc.schedule_day(get_current_date())
     breakpoint()
 
 
