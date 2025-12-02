c08aedbb03620338c97901172cbd63349a8cd649
Todd Sifleet <todd.sifleet@gmail.com>
2025-12-01 21:19:34 -0600

    fix Day.id

--- backend/planned/objects/day.py
+++ backend/planned/objects/day.py
@@ -30,6 +30,9 @@ class Day(BaseObject):
     scheduled_at: datetime | None = None
     alarms: list[Alarm] = Field(default_factory=list)
 
+    def model_post_init(self, __context__=None):
+        self.id = str(self.date)
+
 
 class DayContext(BaseModel):
     date: dt_date
