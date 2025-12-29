430e8aee34b2b34e669543664c016a2bd65638a2
Todd Sifleet <todd.sifleet@gmail.com>
2025-12-28 21:02:02 -0600

    oops

--- backend/planned/application/repositories/day_template_repository.py
+++ backend/planned/application/repositories/day_template_repository.py
@@ -1,10 +1,10 @@
 """Protocol for DayTemplateRepository."""
 
-from planned.application.repositories.base import SimpleReadRepositoryProtocol
+from planned.application.repositories.base import CrudRepositoryProtocol
 from planned.domain.entities import DayTemplate
 
 
-class DayTemplateRepositoryProtocol(SimpleReadRepositoryProtocol[DayTemplate]):
+class DayTemplateRepositoryProtocol(CrudRepositoryProtocol[DayTemplate]):
     """Protocol defining the interface for day template repositories."""
     pass
 
