7e3ece36bed3d45b1e289c02a038ca38a492f2ef
Todd Sifleet <todd.sifleet@gmail.com>
2026-01-07 21:56:51 -0600

    switch to VitePWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,64 @@
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import solidPlugin from "vite-plugin-solid";
 import devtools from "solid-devtools/vite";
-import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin(), tailwindcss()],
+  plugins: [
+    devtools(),
+    solidPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: null,
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.ts",
+      manifestFilename: "manifest.webmanifest",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico", "icons/*"],
+      manifest: {
+        name: "Lykke.Day",
+        short_name: "lykke.day",
+        description: "Intentional daily living",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/maskable-icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icons/maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webp}"],
+      },
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
