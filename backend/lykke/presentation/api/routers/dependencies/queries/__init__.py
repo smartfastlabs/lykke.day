cc4fc24cdfdaea1c17507be102c54a79b881e9fc
Todd Sifleet <todd.sifleet@gmail.com>
2026-01-16 20:22:18 -0600

    checkpoint

--- backend/lykke/presentation/api/routers/dependencies/queries/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Query handler dependency injection functions organized by entity.
-
-This module previously contained wrapper functions for query handlers,
-but these have been replaced by the generic factory functions in
-dependencies.factories.get_query_handler().
-"""
-
