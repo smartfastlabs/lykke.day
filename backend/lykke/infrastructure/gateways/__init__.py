f07f3fee1dcae24d01442cb071875568ff8fc4a7
Todd Sifleet <todd.sifleet@gmail.com>
2026-01-21 20:52:09 -0600

    move from mailgun to sendgrid

--- backend/lykke/infrastructure/gateways/__init__.py
+++ backend/lykke/infrastructure/gateways/__init__.py
@@ -1,9 +1,9 @@
-from . import google, mailgun, redis_pubsub, stub_pubsub, twilio, web_push
+from . import google, redis_pubsub, sendgrid, stub_pubsub, twilio, web_push
 from .anthropic_llm import AnthropicLLMGateway
 from .google import GoogleCalendarGateway
-from .mailgun import MailGunGateway
 from .openai_llm import OpenAILLMGateway
 from .redis_pubsub import RedisPubSubGateway
+from .sendgrid import SendGridGateway
 from .stub_pubsub import StubPubSubGateway
 from .twilio import TwilioGateway
 from .web_push import WebPushGateway
@@ -11,15 +11,15 @@ from .web_push import WebPushGateway
 __all__ = [
     "AnthropicLLMGateway",
     "GoogleCalendarGateway",
-    "MailGunGateway",
     "OpenAILLMGateway",
     "RedisPubSubGateway",
+    "SendGridGateway",
     "StubPubSubGateway",
     "TwilioGateway",
     "WebPushGateway",
     "google",
-    "mailgun",
     "redis_pubsub",
+    "sendgrid",
     "stub_pubsub",
     "twilio",
     "web_push",
